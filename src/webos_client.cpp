4541edb234dcee9054edcc54ba6472ab988cc6f0
Rafael <rafael.seroussi@utexas.edu>
2024-10-28 15:39:00 -0500

    hello world message works on tv

--- src/webos_client.cpp
+++ src/webos_client.cpp
@@ -2,14 +2,21 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <chrono>
+#include <thread>
 #include <nlohmann/json.hpp> // Use a JSON library like nlohmann/json
+#include <endpoints.h>
 #include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
+
+#include <websocketpp/common/thread.hpp>
+#include <websocketpp/common/memory.hpp>
+
 #include <cppcodec/base32_crockford.hpp>
 #include <cppcodec/base64_rfc4648.hpp>
 
 using websocketpp::connection_hdl;
-using client = websocketpp::client<websocketpp::config::asio_client>;
+typedef websocketpp::client<websocketpp::config::asio_client> client;
 
 const std::string KEY_FILE_NAME = ".pylgtv";
 const std::string USER_HOME = "HOME";
@@ -18,6 +25,10 @@ const std::string HANDSHAKE_FILE_NAME = "handshake.json";
 using json = nlohmann::json;
 using base64 = cppcodec::base64_rfc4648;
 using base32 = cppcodec::base32_crockford;
+
+using namespace std::this_thread;     // sleep_for, sleep_until
+using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
+using std::chrono::system_clock;
 
 // Exception class for pairing errors
 class PyLGTVPairException : public std::exception {
@@ -37,7 +48,10 @@ private:
     std::string keyFilePath;
     std::string clientKey;
     client wsClient;
+    websocketpp::lib::shared_ptr<websocketpp::lib::thread> m_thread;
+    client::connection_ptr con;
     int commandCount = 0;
+    json lastResponse;
 
     std::string getKeyFilePath() const {
         const char* home = std::getenv(USER_HOME.c_str());
@@ -48,10 +62,18 @@ private:
     }
 
     void loadKeyFile() {
-        keyFilePath = getKeyFilePath();
-        std::ifstream keyFile(keyFilePath);
+        std::string keyfilep;
+        clientKey = "";
+        if (!keyFilePath.empty()) {
+            keyfilep = keyFilePath;
+        } else {
+            keyfilep = getKeyFilePath();
+        }
+
+        
+        std::ifstream keyFile(keyfilep);
         if (keyFile.fail()) {
-            std::cerr << "Failed to open key file: " << keyFilePath << std::endl;
+            std::cerr << "Failed to open key file: " << keyfilep << std::endl;
             return;
         }
 
@@ -65,22 +87,48 @@ private:
 
     void saveKeyFile() {
         if (clientKey.empty()) return;
+
+        std::string keyfilep;
         
-        std::ofstream keyFile(keyFilePath, std::ios::trunc);
-        if (!keyFile) {
-            std::cerr << "Failed to save key file: " << keyFilePath << std::endl;
+
+        if (!keyFilePath.empty()) {
+            keyfilep = keyFilePath;
+        } else {
+            keyfilep = getKeyFilePath();
+        }
+        std::ifstream keyFilein(keyfilep);
+        
+        
+        if (keyFilein.fail()) {
+            std::cerr << "Failed to open key file: " << keyfilep << std::endl;
             return;
         }
 
         json keyData;
+        keyFilein >> keyData;
+        keyFilein.close();
+
+        std::ofstream keyFileout(keyfilep, std::ios::trunc);
+        if(keyFileout.fail()) {
+            std::cerr << "Failed to open key file: " << keyfilep << std::endl;
+            return;
+        }
+        
         keyData[ip] = clientKey;
-        keyFile << keyData;
-        keyFile.close();
+        keyFileout << keyData.dump();
+        keyFileout.close();
     }
 
 public:
     WebOsClient(const std::string& ip, const std::string& keyFilePath = "") 
         : ip(ip), port(3000), keyFilePath(keyFilePath) {
+        wsClient.clear_access_channels(websocketpp::log::alevel::all);
+        wsClient.clear_error_channels(websocketpp::log::elevel::all);
+        wsClient.init_asio();
+        wsClient.start_perpetual();
+
+        m_thread.reset(new websocketpp::lib::thread(&client::run, &wsClient));
+
         loadKeyFile();
     }
 
@@ -90,7 +138,12 @@ public:
 
     void sendRegisterPayload(websocketpp::connection_hdl hdl) {
         // Load handshake json
-        std::ifstream handshakeFile(HANDSHAKE_FILE_NAME);
+        const char* home = std::getenv(USER_HOME.c_str());
+        std::string handshakefilep = std::string(home) + "/" + HANDSHAKE_FILE_NAME;
+        
+        
+        std::ifstream handshakeFile(handshakefilep);
+        
         if (!handshakeFile) {
             std::cerr << "Failed to open handshake file: " << HANDSHAKE_FILE_NAME << std::endl;
             return;
@@ -99,61 +152,126 @@ public:
         json handshake;
         handshakeFile >> handshake;
         handshake["payload"]["client-key"] = clientKey;
-
+        printf("sending register payload: %s\n", clientKey.c_str());
         // Send JSON over WebSocket
         std::string jsonStr = handshake.dump();
         wsClient.send(hdl, jsonStr, websocketpp::frame::opcode::text);
+        printf("sent register payload\n");
+
     }
 
-    void registerClient() {
+    void _registerClient() {
         try {
-            wsClient.init_asio();
-            wsClient.set_message_handler([this](websocketpp::connection_hdl hdl, client::message_ptr msg) {
+                // wsClient.set_message_handler([this](websocketpp::connection_hdl hdl, client::message_ptr msg) {
+                // json response;
+                // std::istringstream(msg->get_payload()) >> response;
+                // if (response["type"].get<std::string>() == "registered") {
+                //     clientKey = response["payload"]["client-key"].get<std::string>();
+                //     saveKeyFile();
+                //     std::cout << "saved key file" << std::endl;
+                // }
+            // });
+
+            std::string uri = "ws://" + ip + ":" + std::to_string(port);
+            websocketpp::lib::error_code ec;
+            con = wsClient.get_connection(uri, ec);
+            if (ec) {
+                std::cerr << "Failed to create connection: " << ec.message() << std::endl;
+                return;
+            }
+            con->set_open_handler([this](websocketpp::connection_hdl hdl) {
+                sendRegisterPayload(hdl);
+            });
+            con->set_fail_handler([this](websocketpp::connection_hdl hdl) {
+                std::cerr << "Failed to connect to TV" << std::endl;
+            });
+            con->set_message_handler([this](websocketpp::connection_hdl hdl, client::message_ptr msg) {
                 json response;
                 std::istringstream(msg->get_payload()) >> response;
                 if (response["type"].get<std::string>() == "registered") {
                     clientKey = response["payload"]["client-key"].get<std::string>();
                     saveKeyFile();
+                    std::cout << "saved key file" << std::endl;
+                } else {
+                    std::cout << "Received message: " << response.dump() << std::endl;
                 }
+                lastResponse = response;
             });
-
-            std::string uri = "ws://" + ip + ":" + std::to_string(port);
-            websocketpp::lib::error_code ec;
-            auto con = wsClient.get_connection(uri, ec);
-            if (ec) {
-                std::cerr << "Failed to create connection: " << ec.message() << std::endl;
-                return;
-            }
-
             wsClient.connect(con);
-            wsClient.run();
+            printf("Connected to TV\n");
+            sleep_for(5s);
+            sendRegisterPayload(con->get_handle());
+            printf("Sent register payload\n");
+
+            
 
         } catch (const std::exception& e) {
             std::cerr << "Error during registration: " << e.what() << std::endl;
         }
     }
 
-    void sendCommand(const std::string& requestType, const std::string& uri, const json& payload) {
+    void registerClient() {
+        int attempts = 0;
+        while(clientKey.empty()) {
+            _registerClient();
+            if (attempts++ > 5) {
+                throw PyLGTVPairException("pairing-failed", "Failed to pair with TV");
+                return;
+            }
+            sleep_for(100ms);
+            
+        }
+        std::cout << "Successfully paired with TV" << std::endl;
+    }
+
+    void _sendCommand(const json& msg) {
         commandCount++;
+        // json message;
+        // message["id"] = requestType + "_" + std::to_string(commandCount);
+        // message["type"] = requestType;
+        // message["uri"] = "ssap://" + uri;
+        // message["payload"] = payload;
+
+        // Send JSON over WebSocket
+        std::string jsonStr = msg.dump();
+        wsClient.send(con->get_handle(), jsonStr, websocketpp::frame::opcode::text);
+    }
+
+    void sendCommand(const std::string& requestType, const std::string& uri, const json& payload) {
         json message;
         message["id"] = requestType + "_" + std::to_string(commandCount);
         message["type"] = requestType;
         message["uri"] = "ssap://" + uri;
         message["payload"] = payload;
+        lastResponse = json();
+        _sendCommand(message);
+    }
 
-        // Send JSON over WebSocket
-        std::string jsonStr = message.dump();
-        wsClient.send(websocketpp::connection_hdl(), jsonStr, websocketpp::frame::opcode::text);
+    void request(const std::string& uri, const json& payload) {
+        sendCommand("request", uri, payload);
+    }
 
+    void send_message(std::string message) {
+        json payload;
+        payload["message"] = message;
+        payload["iconData"] = "";
+        payload["iconExtension"] = "";
+
+        request(EP_SHOW_MESSAGE, payload);
     }
 };
 
 // Example usage:
 int main() {
-    WebOsClient client("192.168.0.10");
+    WebOsClient client("192.168.86.41");
+    printf("Client created\n");
     if (!client.isRegistered()) {
         client.registerClient();
     }
+
+    //send a command to the tv
+    printf("Sending message\n");
+    client.send_message("Hello, World!");
 
     // Further logic to send commands or interact with the TV...
     return 0;
