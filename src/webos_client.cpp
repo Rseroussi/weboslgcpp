cd6c0143e3e4c0dc34330d00302045b3d150b1da
Rafael <rafael.seroussi@utexas.edu>
2024-10-29 13:38:42 -0500

    fixed waiting for reply message issues

--- src/webos_client.cpp
+++ src/webos_client.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <filesystem>
 #include <chrono>
 #include <thread>
 #include <nlohmann/json.hpp> // Use a JSON library like nlohmann/json
@@ -15,10 +16,15 @@
 #include <cppcodec/base32_crockford.hpp>
 #include <cppcodec/base64_rfc4648.hpp>
 
+#define WFR_SLEEP_TIME_MS 200000 //the sleep time in microseconds to wait for a response from the tv until checking again for a response. This is used with WAIT_FOR_RESPONSE. 
+#define WAIT_FOR_RESPONSE 10 //this is used with WFR_SLEEP_TIME_MS to wait for a response from the tv. So the max time you will wait is WAIT_FOR_RESPONSE * WFR_SLEEP_TIME_MS after registration
+
+#define REGISTRATION_TIMEOUT 20 //the max time to wait for registration in seconds
+#define REGISTRATION_RETRY_INTERVAL 5 //the time to wait before retrying registration in seconds
 using websocketpp::connection_hdl;
 typedef websocketpp::client<websocketpp::config::asio_client> client;
 
-const std::string KEY_FILE_NAME = ".pylgtv";
+const std::string KEY_FILE_NAME = ".cpplgtv";
 const std::string USER_HOME = "HOME";
 const std::string HANDSHAKE_FILE_NAME = "handshake.json";
 
@@ -119,21 +125,42 @@ private:
         keyFileout.close();
     }
 
+    //to use this function you must set lastResponse to empty before calling it. you need to set this before you send the request
+    void wait_for_response() {
+        int wait = 0;
+        while (lastResponse.empty() || wait++ < WAIT_FOR_RESPONSE) {
+            usleep(WFR_SLEEP_TIME_MS);
+        }
+    }
+
 public:
-    WebOsClient(const std::string& ip, const std::string& keyFilePath = "") 
-        : ip(ip), port(3000), keyFilePath(keyFilePath) {
+    WebOsClient(const std::string& ip, const std::string& keyFilePathi = "") 
+        : ip(ip), port(3000), keyFilePath(keyFilePathi) {
         wsClient.clear_access_channels(websocketpp::log::alevel::all);
         wsClient.clear_error_channels(websocketpp::log::elevel::all);
         wsClient.init_asio();
         wsClient.start_perpetual();
-
+        
         m_thread.reset(new websocketpp::lib::thread(&client::run, &wsClient));
 
         loadKeyFile();
+        if(keyFilePathi.empty()) {
+            keyFilePath = getKeyFilePath();
+        }
+        std::ofstream keyFile(keyFilePath);
+        keyFile << "{}";
+        keyFile.close();
+        
     }
 
     bool isRegistered() const {
         return !clientKey.empty();
+    }
+    ~WebOsClient() {
+        wsClient.stop_perpetual();
+        wsClient.stop();
+        m_thread->join();
+        std::filesystem::remove(keyFilePath);
     }
 
     void sendRegisterPayload(websocketpp::connection_hdl hdl) {
@@ -162,15 +189,6 @@ public:
 
     void _registerClient() {
         try {
-                // wsClient.set_message_handler([this](websocketpp::connection_hdl hdl, client::message_ptr msg) {
-                // json response;
-                // std::istringstream(msg->get_payload()) >> response;
-                // if (response["type"].get<std::string>() == "registered") {
-                //     clientKey = response["payload"]["client-key"].get<std::string>();
-                //     saveKeyFile();
-                //     std::cout << "saved key file" << std::endl;
-                // }
-            // });
 
             std::string uri = "ws://" + ip + ":" + std::to_string(port);
             websocketpp::lib::error_code ec;
@@ -192,15 +210,26 @@ public:
                     clientKey = response["payload"]["client-key"].get<std::string>();
                     saveKeyFile();
                     std::cout << "saved key file" << std::endl;
-                } else {
-                    std::cout << "Received message: " << response.dump() << std::endl;
-                }
+                } 
+                
+                // else {
+                //     std::cout << "Received message: " << response.dump() << std::endl;
+                // }
                 lastResponse = response;
             });
+
             wsClient.connect(con);
             printf("Connected to TV\n");
-            sleep_for(5s);
+            
+            
             sendRegisterPayload(con->get_handle());
+
+            // sleep_for(1s);
+            // //wait for registration response
+            // while(clientKey.empty()) {
+            //     sleep_for(200ms);
+            // }
+
             printf("Sent register payload\n");
 
             
@@ -213,24 +242,22 @@ public:
     void registerClient() {
         int attempts = 0;
         while(clientKey.empty()) {
-            _registerClient();
-            if (attempts++ > 5) {
+            
+            if (attempts++ % REGISTRATION_RETRY_INTERVAL == 0) {
+                _registerClient();
+            } else if(attempts > REGISTRATION_TIMEOUT) {
                 throw PyLGTVPairException("pairing-failed", "Failed to pair with TV");
                 return;
             }
-            sleep_for(100ms);
+            sleep_for(1s);
             
         }
+        
         std::cout << "Successfully paired with TV" << std::endl;
     }
 
     void _sendCommand(const json& msg) {
         commandCount++;
-        // json message;
-        // message["id"] = requestType + "_" + std::to_string(commandCount);
-        // message["type"] = requestType;
-        // message["uri"] = "ssap://" + uri;
-        // message["payload"] = payload;
 
         // Send JSON over WebSocket
         std::string jsonStr = msg.dump();
@@ -261,13 +288,22 @@ public:
     }
 
     //Apps
+    json response() {
+        return lastResponse;
+    }
+
+
     json listApps() {
+        lastResponse = json();
         request(EP_GET_APPS, json());
+        wait_for_response();
         return lastResponse["payload"]["launchPoints"];
     }
 
     json getCurrentApp() {
+        lastResponse = json();
         request(EP_GET_CURRENT_APP_INFO, json());
+        wait_for_response();
         return lastResponse["payload"]["appId"];
     }
 
@@ -299,12 +335,16 @@ public:
 
     //services
     json listServices() {
+        lastResponse = json();
         request(EP_GET_SERVICES, json());
+        wait_for_response();
         return lastResponse["payload"]["services"];
     }
 
     json getSoftwareInfo() {
+        lastResponse = json();
         request(EP_GET_SOFTWARE_INFO, json());
+        wait_for_response();
         return lastResponse["payload"];
     }
 
@@ -326,7 +366,9 @@ public:
     
     //Inputs
     json listInputs() {
+        lastResponse = json();
         request(EP_GET_INPUTS, json());
+        wait_for_response();
         return lastResponse["payload"]["devices"];
     }
 
@@ -343,7 +385,9 @@ public:
     //audio
 
     json getVolume() {
+        lastResponse = json();
         request(EP_GET_VOLUME, json());
+        wait_for_response();
         return lastResponse["payload"];
     }
 
@@ -363,12 +407,16 @@ public:
 
     //channels
     json getChannels() {
+        lastResponse = json();
         request(EP_GET_TV_CHANNELS, json());
+        wait_for_response();
         return lastResponse["payload"]["channelList"];
     }
 
     json getCurrentChannel() {
+        lastResponse = json();
         request(EP_GET_CURRENT_CHANNEL, json());
+        wait_for_response();
         return lastResponse["payload"];
     }
 
@@ -381,7 +429,9 @@ public:
     json getChannelInfo(const std::string& channelId) {
         json payload;
         payload["channelId"] = channelId;
+        lastResponse = json();
         request(EP_GET_CHANNEL_INFO, payload);
+        wait_for_response();
         return lastResponse["payload"];
     }
 
@@ -445,13 +495,17 @@ int main() {
     }
 
     //send a command to the tv
-    printf("Sending message\n");
-    client.send_message("Hello, World!");
+    // client.powerOn();
+    // printf("Sending message\n");
+    // client.send_message("Hello, World!");
 
-    for(int i = 0; i < 6 ; i++) {
-        client.volumeUp();
-        sleep_for(500ms);
-    }
+    // for(int i = 0; i < 6 ; i++) {
+    //     client.volumeUp();
+    //     sleep_for(500ms);
+    // }
+    
+    std::cout << client.listApps().dump() << std::endl;
+    
 
     // Further logic to send commands or interact with the TV...
     return 0;
